ee222ea5cc76f0e32ae8e36d1c920739c5f51d3e
ainyfadila <ainyfadila@gmail.com>
2020-01-15 09:13:00 +0700

    project quasar

---
+++ src/router/routes.js
@@ -0,0 +1,25 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '',
+        component: () => import('pages/ListTodo.vue')
+      },
+      { path: '',
+        component: () => import('pages/PageSetting.vue')
+      }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
